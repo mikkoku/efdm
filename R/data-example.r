b75b60394094444d2a9d23bf8215dd4d21a3ed58
mikkoku <mikko.p.kuronen@gmail.com>
2021-03-31 17:55:18 +0300

    Add vignette

---
+++ R/data-example.r
@@ -0,0 +1,15 @@
+#' Example dataset.
+#'
+#' A listt containing the necessary datasets for EFDM forest scenario example.
+#'
+#' @format A list of data frames:
+#' \describe{
+#'   \item{actprob}{Activity probabilities}
+#'   \item{noman_pairs}{Pair data for growth without management activities}
+#'   \item{thin_pairs}{Pair data for thinning}
+#'   \item{initial_state}{Initial forest state}
+#'   \item{drain_coef}{Coefficient to transform harvested areas into harvest accumulation by timber assortments}
+#'   \item{vol_coef}{Coefficients to transform volume classes into volumes m3/ha}
+#'   \item{income_coef}{Coefficients to transform harvest accumulation into income}
+#' }
+"example"
